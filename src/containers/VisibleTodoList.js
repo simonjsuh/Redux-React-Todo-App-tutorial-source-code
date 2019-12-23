fb6c596505a992ea5aba25b7813f3fe825ec775e
simonjsuh <simon.j.suh@gmail.com>
2019-12-23 10:42:09 -0800

    first commit

---
+++ src/containers/VisibleTodoList.js
@@ -0,0 +1,29 @@
+import TodoList from '../components/TodoList';
+import { connect } from 'react-redux';
+import { VisibilityFilters, toggleTodo } from '../actions';
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(task => task.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(task => !task.completed);
+    default:
+      throw new Error('Unknown Filter: ' + filter);
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+});
+
+const mapDispatchToProps = dispatch => ({
+  toggleTodo: id => dispatch(toggleTodo(id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
